b219db1a1f2cdf2d8ef20597cfa76678c4172102
Nate <ebling.nathan@gmail.com>
2024-02-13 09:48:30 -0500

    Added changeScene method; initialize a scene; added current scene update to loop

--- src/main/java/cookie/Window.java
+++ src/main/java/cookie/Window.java
@@ -19,6 +19,8 @@ public class Window {
 
     private static Window window = null;
 
+    private static Scene currentScene;
+
     private Window() {
         this.width = 1920;
         this.height = 1080;
@@ -27,6 +29,22 @@ public class Window {
         g = 1;
         b = 1;
         a = 1;
+    }
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                //currentScene.init();
+                break;
+            default:
+                assert false : "Unknown scene '" + newScene + "'";
+                break;
+        }
     }
 
     public static Window get() {
@@ -90,11 +108,14 @@ public class Window {
 
         GL.createCapabilities();
 
+        Window.changeScene(0);
+
     }
 
     public void loop() {
         float beginTime = Time.getTime();
         float endTime = Time.getTime();
+        float dt = -1.0f;
 
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events
@@ -103,14 +124,14 @@ public class Window {
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
-                System.out.println("Space is pressed");
+            if (dt >= 0) {
+                currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
 
             endTime = Time.getTime();
-            float dt = endTime - beginTime;
+            dt = endTime - beginTime;
             beginTime = endTime;
         }
     }

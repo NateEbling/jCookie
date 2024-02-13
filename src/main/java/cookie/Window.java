4ddf0ebb8639e52aaef87bddc47068e940ecec7e
Nate <ebling.nathan@gmail.com>
2024-02-13 08:54:14 -0500

    Set key callback and added default r, g, b, and a values

--- src/main/java/cookie/Window.java
+++ src/main/java/cookie/Window.java
@@ -14,12 +14,18 @@ public class Window {
     private String title;
     private long glfwWindow;
 
+    private float r, g, b, a;
+
     private static Window window = null;
 
     private Window() {
         this.width = 1920;
         this.height = 1080;
         this.title = "My Game";
+        r = 1;
+        g = 1;
+        b = 1;
+        a = 1;
     }
 
     public static Window get() {
@@ -70,6 +76,7 @@ public class Window {
         glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
         glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
         // Make OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -89,8 +96,12 @@ public class Window {
             // Poll events
             glfwPollEvents();
 
-            glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                System.out.println("Space is pressed");
+            }
 
             glfwSwapBuffers(glfwWindow);
         }

2c380fb5ae20506306b891a6044c550357187ae3
Nate <ebling.nathan@gmail.com>
2024-02-13 09:41:59 -0500

    Added two Scenes that extend Scene.java

---
+++ src/main/java/cookie/LevelEditorScene.java
@@ -0,0 +1,15 @@
+package cookie;
+
+import java.awt.event.KeyEvent;
+
+public class LevelEditorScene extends Scene {
+
+    public LevelEditorScene() {
+        System.out.println("Inside LevelEditorScene");
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
---
+++ src/main/java/cookie/LevelScene.java
@@ -0,0 +1,16 @@
+package cookie;
+
+import java.awt.event.KeyEvent;
+
+public class LevelScene extends Scene{
+
+
+    public LevelScene() {
+        System.out.println("Inside LevelScene");
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}

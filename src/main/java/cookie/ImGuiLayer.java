3835623d38712d0922b53f7a42f4d3648a670f35
Nate <ebling.nathan@gmail.com>
2024-03-08 08:14:33 -0500

    Added ImGui library and ImGuiLayer.java

---
+++ src/main/java/cookie/ImGuiLayer.java
@@ -0,0 +1,103 @@
+package cookie;
+
+import imgui.*;
+import imgui.callback.ImStrConsumer;
+import imgui.callback.ImStrSupplier;
+import imgui.flag.ImGuiConfigFlags;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+
+public class ImGuiLayer {
+
+    private long glfwWindow;
+
+    public void initImGui() {
+        // Critical for ImGui to work
+        ImGui.createContext();
+
+        // Initialize ImGuiIO config
+        final ImGuiIO io = ImGui.getIO();
+
+        io.setIniFilename("imgui.ini"); // We don't want to save .ini file
+        io.addConfigFlags(ImGuiConfigFlags.DockingEnable);
+        io.addConfigFlags(ImGuiConfigFlags.ViewportsEnable);
+        io.setBackendPlatformName("imgui_java_impl_glfw");
+
+        // ------------------------------------------------------------
+        // GLFW callbacks to handle user input
+
+        glfwSetKeyCallback(glfwWindow, (w, key, scancode, action, mods) -> {
+            if (action == GLFW_PRESS) {
+                io.setKeysDown(key, true);
+            } else if (action == GLFW_RELEASE) {
+                io.setKeysDown(key, false);
+            }
+
+            io.setKeyCtrl(io.getKeysDown(GLFW_KEY_LEFT_CONTROL) || io.getKeysDown(GLFW_KEY_RIGHT_CONTROL));
+            io.setKeyShift(io.getKeysDown(GLFW_KEY_LEFT_SHIFT) || io.getKeysDown(GLFW_KEY_RIGHT_SHIFT));
+            io.setKeyAlt(io.getKeysDown(GLFW_KEY_LEFT_ALT) || io.getKeysDown(GLFW_KEY_RIGHT_ALT));
+            io.setKeySuper(io.getKeysDown(GLFW_KEY_LEFT_SUPER) || io.getKeysDown(GLFW_KEY_RIGHT_SUPER));
+
+            if (!io.getWantCaptureKeyboard()) {
+                KeyListener.keyCallback(w, key, scancode, action, mods);
+            }
+        });
+        glfwSetCharCallback(glfwWindow, (w, c) -> {
+            if (c != GLFW_KEY_DELETE) {
+                io.addInputCharacter(c);
+            }
+        });
+
+        glfwSetMouseButtonCallback(glfwWindow, (w, button, action, mods) -> {
+            final boolean[] mouseDown = new boolean[5];
+
+            mouseDown[0] = button == GLFW_MOUSE_BUTTON_1 && action != GLFW_RELEASE;
+            mouseDown[1] = button == GLFW_MOUSE_BUTTON_2 && action != GLFW_RELEASE;
+            mouseDown[2] = button == GLFW_MOUSE_BUTTON_3 && action != GLFW_RELEASE;
+            mouseDown[3] = button == GLFW_MOUSE_BUTTON_4 && action != GLFW_RELEASE;
+            mouseDown[4] = button == GLFW_MOUSE_BUTTON_5 && action != GLFW_RELEASE;
+
+            io.setMouseDown(mouseDown);
+
+            if (!io.getWantCaptureMouse() && mouseDown[1]) {
+                ImGui.setWindowFocus(null);
+            }
+
+
+//            if (gameViewWindow.getWantCaptureMouse()) {
+//                MouseListener.mouseButtonCallback(w, button, action, mods);
+//            }
+        });
+
+//        glfwSetScrollCallback(glfwWindow, (w, xOffset, yOffset) -> {
+//            io.setMouseWheelH(io.getMouseWheelH() + (float) xOffset);
+//            io.setMouseWheel(io.getMouseWheel() + (float) yOffset);
+//            if (!io.getWantCaptureMouse() || gameViewWindow.getWantCaptureMouse()) {
+//                MouseListener.mouseScrollCallback(w, xOffset, yOffset);
+//            } else {
+//                MouseListener.clear();
+//            }
+//        });
+
+        io.setSetClipboardTextFn(new ImStrConsumer() {
+            @Override
+            public void accept(final String s) {
+                glfwSetClipboardString(glfwWindow, s);
+            }
+        });
+
+        io.setGetClipboardTextFn(new ImStrSupplier() {
+            @Override
+            public String get() {
+                final String clipboardString = glfwGetClipboardString(glfwWindow);
+                if (clipboardString != null) {
+                    return clipboardString;
+                } else {
+                    return "";
+                }
+            }
+        });
+
+    }
+}
